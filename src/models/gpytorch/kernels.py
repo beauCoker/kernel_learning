425f119db394f6b88c823e67d9149589a3102f27
Beau Coker <beaucoker@dhcp-10-250-213-11.harvard.edu>
2023-03-28 14:33:14 -0400

    add priors

--- src/models/gpytorch/kernels.py
+++ src/models/gpytorch/kernels.py
@@ -16,31 +16,27 @@ def make_kernel(**kwargs):
 
     # priors
     if 'ls_prior' in kwargs:
-        ls_prior = UniformPrior(1, 2)
-        ls_constraint = gpytorch.constraints.Interval(1, 2)
+        ls_prior, ls_constraint = make_prior(kwargs['ls_prior'])
     else:
-        ls_prior = None
-        ls_constraint = None
+        ls_prior, ls_constraint = None, None
 
     if 'var_prior' in kwargs:
-        var_prior =  UniformPrior(1, 2)
-        var_constraint = gpytorch.constraints.Interval(1, 2)
+        var_prior, var_constraint = make_prior(kwargs['var_prior'])
     else:
-        var_prior = None
-        var_constraint = None
+        var_prior, var_constraint = None, None
 
     # kernel
     if kwargs['name'] == 'rbf':
-        kernel = gpytorch.kernels.RBFKernel(lengthscale_prior = ls_prior)
+        kernel = gpytorch.kernels.RBFKernel(lengthscale_prior = ls_prior, lengthscale_constraint = ls_constraint)
 
     elif kwargs['name'] == 'matern12':
         kernel = gpytorch.kernels.MaternKernel(nu=1/2, lengthscale_prior = ls_prior, lengthscale_constraint = ls_constraint)
 
     elif kwargs['name'] == 'matern32':
-        kernel = gpytorch.kernels.MaternKernel(nu=3/2, lengthscale_prior = ls_prior)
+        kernel = gpytorch.kernels.MaternKernel(nu=3/2, lengthscale_prior = ls_prior, lengthscale_constraint = ls_constraint)
 
     elif kwargs['name'] == 'matern52':
-        kernel = gpytorch.kernels.MaternKernel(nu=5/2, lengthscale_prior = ls_prior)
+        kernel = gpytorch.kernels.MaternKernel(nu=5/2, lengthscale_prior = ls_prior, lengthscale_constraint = ls_constraint)
 
     # always add variance
     kernel = gpytorch.kernels.ScaleKernel(kernel,  outputscale_prior=var_prior, outputscale_constraint=var_constraint)
@@ -53,4 +49,18 @@ def make_kernel(**kwargs):
         kernel.outputscale = kwargs['var']
 
     return kernel
+
+def make_prior(name):
+    if name == 'uniform':
+        prior = gpytorch.priors.UniformPrior(.1, 10)
+        constraint = gpytorch.constraints.Interval(.1, 10)
+
+    elif name == 'gamma':
+        prior = gpytorch.priors.GammaPrior(.1, .1)
+        constraint = gpytorch.constraints.Positive()
+
+    else:
+        raise ValueError('Prior not found')
+
+    return prior, constraint
 
